2584df02dba2cfb52f2267f475f72d3256b8cde8
sergey200312 <serezha.mikhaylov.03@list.ru>
2024-09-06 21:39:31 +0500

    подкорректировал контроллеры

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -9,12 +9,13 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 exports.register = asyncHandler(async (req, res, next) => {
-    const { username, email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
-        username,
+        firstName,
+        lastName,
         email,
         password: hashedPassword,
     });
@@ -65,18 +66,18 @@ exports.verifyEmail = asyncHandler(async(req, res, next) => {
 })
 
 exports.login = asyncHandler(async (req, res, next) => {
-    const { login, password } = req.body;
+    const { email, password } = req.body;
 
-    const user = await User.findOne({ username: login }).exec();
+    const user = await User.findOne({ email: email }).exec();
 
     if (!user) {
-        return res.status(400).json({ message: "Некорректный логин или пароль" })
+        return res.status(400).json({ message: "Некорректный email или пароль" })
     }
 
     const match = await bcrypt.compare(password, user.password);
 
     if (!match) {
-        return res.status(400).json({ message: "Некорректный логин или пароль" });
+        return res.status(400).json({ message: "Некорректный email или пароль" });
     }
 
     const token = jwt.sign(
