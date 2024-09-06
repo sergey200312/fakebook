0c83e203ee4dc7646e43e7b168700285df717009
sergey200312 <serezha.mikhaylov.03@list.ru>
2024-09-06 21:31:57 +0500

    убрал юзернейм из модели

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {
+    firstName: {
+        type: String,
+        required: true, 
+        trim: true
+    },
+    lastName: {
         type: String,
         required: true,
-        trim: true,
-        unique: true
+        trim: true
     },
     email: {
         type: String,
@@ -23,11 +27,6 @@ const UserSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    },
-    role: {
-        type: String,
-        enum: ["USER", "ADMIN"],
-        default: "USER"
     },
     posts: [{
         type: Schema.Types.ObjectId,
