f4855cc27d68d270990833d69a767132317c03e0
sergey200312 <serezha.mikhaylov.03@list.ru>
2024-09-06 21:20:48 +0500

    оформил стили для регистрации

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -2,42 +2,42 @@ import React from 'react'
 
 export default function RegisterPage() {
     return (
-        <div className='flex justify-center items-center min-h-screen'>
-            <div className='shadow-lg p-8 max-w-sm bg-white w-full'>
-                <h1 className='text-center font-bold text-2xl mb-6'>Регистрация</h1>
+        <div className='flex justify-center items-center min-h-screen bg-cover bg-center'>
+            <div className='shadow-[#2fc481] p-8 max-w-sm shadow-2xl w-full bg-white rounded-2xl'>
+                <h1 className='text-center text-2xl mb-6'>Регистрация</h1>
                 <form>
                     <div className='mb-4'>
                         <input
                             type='text'
                             id='username'
                             placeholder='Имя'
-                            className='w-full border-neutral-950 border-b-2 focus:outline-none' />
+                            className='w-full border-lime-500 focus:border-lime-600 border-b-2 focus:outline-none' />
                     </div>
                     <div className='mb-4'>
                         <input
                             type='text'
                             id='email'
                             placeholder='Email'
-                            className='w-full border-neutral-950 border-b-2 focus:outline-none' />
+                            className='w-full border-lime-500 focus:border-lime-600 border-b-2 focus:outline-none' />
                     </div>
                     <div className='mb-4'>
                         <input
                             type='password'
                             id='password'
                             placeholder='Пароль'
-                            className='w-full border-neutral-950 border-b-2 focus:outline-none' />
+                            className='w-full border-lime-500 focus:border-lime-600  border-b-2 focus:outline-none' />
                     </div>
                     <div className='mb-8'>
                         <input
                             type='password'
                             id='confirm-password'
                             placeholder='Повторите пароль'
-                            className='w-full border-neutral-950 border-b-2 focus:outline-none' />
+                            className='w-full border-lime-500 focus:border-lime-600 border-b-2 focus:outline-none' />
                     </div>
                     <div className='flex justify-center'>
                         <button
                             type='submit'
-                            className='bg-gray-950 text-white p-1 rounded-lg hover:bg-gray-700'>Зарегистрироваться</button>
+                            className='bg-[#2fc481] text-white p-1 rounded-lg hover:shadow-lg'>Зарегистрироваться</button>
                     </div>
                 </form>
             </div>
