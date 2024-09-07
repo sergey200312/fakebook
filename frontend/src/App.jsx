c6901260347caaaf205e45ab6be1a3a962359828
sergey200312 <serezha.mikhaylov.03@list.ru>
2024-09-08 00:07:36 +0500

    протестировал работоспособность добавления в защищенный маршрут

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,19 @@ import './App.css'
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import VerificationEmailPage from './pages/VerificationEmailPage';
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path='/register' element={<RegisterPage/>} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/verify' element={<VerificationEmailPage/>} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/verify' element={<VerificationEmailPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<h1>dgdg</h1>} />
+        </Route>
       </Routes>
     </Router>
   )
--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 export default function PrivateRoute() {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  const navigate = useNavigate();
   
-  return isAuthenticated ? <Outlet /> : navigate('/login');
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />;
 }
