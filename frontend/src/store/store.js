f4cb7e9743d138912a2d3ee84ab7a96d57033225
sergey200312 <serezha.mikhaylov.03@list.ru>
2024-09-12 21:00:07 +0500

    добавил срез userpi в стор

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../features/authSlice';
+import { userApi } from "../features/userApi";
 
 const store = configureStore({
     reducer: {
-        auth: authReducer
-    }
-})
+        auth: authReducer,
+        [userApi.reducerPath]: userApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(userApi.middleware)
+});
 export default store;
